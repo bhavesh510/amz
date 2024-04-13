909075365b3563fb8cf2383ecd248bab846d9d23
bhavesh510 <bspatil611@gmail.com>
2024-04-13 17:45:45 +0530

    added file

--- web_app/src/main/java/web_app/Webapplication.java
+++ web_app/src/main/java/web_app/Webapplication.java
@@ -1,27 +1,26 @@
 package web_app;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Webapplication {
+public class WebApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.Firefox.driver","C:\\Users\\exam\\Desktop\\web_application\\geckodriver\\geckodriver.exe");
-		WebDriver driver =new FirefoxDriver();
-		driver.get("https://www.swiggy.com");
-		driver.findElement(By.id("APjFqb")).sendKeys("https://www.swiggy.com");
-		//driver.findElement(By.name("btnK")).click();
-		driver.findElement(By.xpath("//input[@class = 'gNO89b']")).click();	
-		driver.findElement(By.className("gNO89b")).click();
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\exam\\Desktop\\web_application\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=15");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("shoes for men");
+		Thread.sleep(2000);
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("s-image")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("class-button-input")).click();
+		Thread.sleep(2000);
+	}
 	
-	}
-		
 
-		
-	}
-
-
+}
